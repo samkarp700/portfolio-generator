bde4585cdfdbda32aed16dd826d3d4829adda921
Sam Karp <sfcypret@gmail.com>
2022-08-06 15:28:44 -0400

    prompt working

--- app.js
+++ app.js
@@ -1,24 +1,19 @@
-const FileSystem = require('fs');
-const profileDataArgs = process.argv.slice(2);
-const [name, github] = profileDataArgs;
-const generatePage = require('./src/page-template.js');
+const inquirer = require('inquirer');
+inquirer
+    .prompt([
+        {
+            type: 'input', 
+            name: 'name', 
+            message: 'What is your name?'
+        }
+    ])
+    .then(answers => console.log(answers));
+// const FileSystem = require('fs');
+// const generatePage = require('./src/page-template.js');
+// const pageHTML = generatePage(name, github);
 
-FileSystem.writeFile('./index.html', generatePage(name,github), err => {
-    if (err) throw new Error(err);
+// FileSystem.writeFile('./index.html', pageHTML, err => {
+//     if (err) throw (err);
 
-    console.log('Portfolio Complete! Check out index.html to see the output!');
-});
-
-// const printProfileData = profileDataArr => {
-//     //This...
-//     for (let i=0; i< profileDataArr.length; i += 1) {
-//         console.log(profileDataArr[i]);
-//     }
-
-//     console.log('==================');
-
-//     //is the same as this...
-//     profileDataArr.forEach(profileItem => console.log(profileItem));
-// };
-
-// printProfileData(profileDataArgs);
+//     console.log('Portfolio Complete! Check out index.html to see the output!');
+// });
