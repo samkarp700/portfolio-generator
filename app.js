ea7c6f0d55b965087c0ebbef00f9a0028042b1f5
Sam Karp <sfcypret@gmail.com>
2022-08-06 14:30:55 -0400

    feature generate webpage

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
-console.log("Hello Node!");
+const FileSystem = require('fs');
+const profileDataArgs = process.argv.slice(2);
+const [name, github] = profileDataArgs;
+const generatePage = require('./src/page-template.js');
+
+FileSystem.writeFile('./index.html', generatePage(name,github), err => {
+    if (err) throw new Error(err);
+
+    console.log('Portfolio Complete! Check out index.html to see the output!');
+});
+
+// const printProfileData = profileDataArr => {
+//     //This...
+//     for (let i=0; i< profileDataArr.length; i += 1) {
+//         console.log(profileDataArr[i]);
+//     }
+
+//     console.log('==================');
+
+//     //is the same as this...
+//     profileDataArr.forEach(profileItem => console.log(profileItem));
+// };
+
+// printProfileData(profileDataArgs);
