a844dede4aa3ae1c3e9a3243a268ee68e7dcf592
Sam Karp <sfcypret@gmail.com>
2022-08-07 22:44:08 -0400

    html done

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const inquirer = require('inquirer');
 const generatePage = require('./src/page-template');
 
 
+
+
 const promptUser = () => {
     return inquirer.prompt([
         {
@@ -143,6 +145,12 @@ const promptProject = portfolioData => {
 promptUser()
     .then(promptProject)
     .then(portfolioData => {
-        console.log(portfolioData);
+        const pageHTML = generatePage(portfolioData);
+
+        fs.writeFile('./index.html', pageHTML, err => {
+            if (err) throw new Error(err);
+        console.log('Page created! Check out index.html in this directory to see it!');
+        });
+        
     });
 
